5faddfe4cbb8b92e61e52bbb0a6c7dd8ae6339c8
Nikita Fedorov <nikitafedorov666@icloud.com>
2021-11-27 23:07:33 +0300

    update

--- #src/js/script.js
+++ #src/js/script.js
@@ -134,12 +134,6 @@ document.addEventListener('DOMContentLoaded', () => {
     
     //sliders
     
-    // class Slider {
-    //     constructor() {
-    
-    //     }
-    // }
-    
     // function sliderMain(wrapperSelector, fieldSelector, slidesSelector, leftSelector, rightSelector, nameClasss,speed) {
     //     const sliderWrapper = document.querySelector(wrapperSelector)
     //     const sliderField = document.querySelector(fieldSelector)
@@ -207,89 +201,225 @@ document.addEventListener('DOMContentLoaded', () => {
         
     // }
     
-    function sliderSmall(fieldSelector, slidesSelector, leftSelector, rightSelector, speed, wrapperSelector = '', adaptiv = false) {
+    // function sliderSmall(fieldSelector, slidesSelector, leftSelector, rightSelector, speed, wrapperSelector = '', adaptiv = false) {
+    //     const sliderField = document.querySelector(fieldSelector)
+    //     const slides = document.querySelectorAll(slidesSelector)
+    //     const leftBtn = document.querySelector(leftSelector)
+    //     const rightBtn = document.querySelector(rightSelector)
+    //     const wrapper = document.querySelector(wrapperSelector)
+    //     let step = 0
+    //     let widthSlides = slides[0].clientWidth * slides.length
+
+    //     if(adaptiv) {
+    //         step = wrapper.clientWidth
+    //         widthSlides = step * slides.length
+    //         window.addEventListener('resize', () => {
+    //             step = wrapper.clientWidth
+
+    //             widthSlides = step * slides.length
+
+    //             sliderField.style.width = `${step * (slides.length + 2)}px`
+    //             sliderField.style.transform = `translateX(${-step}px)`
+    //         })
+            
+    //     } else {
+    //         step = slides[0].clientWidth
+    //         window.addEventListener('resize', () => {
+    //             step = slides[0].clientWidth
+    //             sliderField.style.transform = `translateX(${-step}px)`
+    //         })
+    //     }
+        
+    //     if(widthSlides >= (wrapper.clientWidth)) {
+            
+    //         const elemLast = slides[0].cloneNode(true)
+    //         sliderField.append(elemLast)
+        
+    //         const elemFirst = slides[slides.length - 1].cloneNode(true)
+    //         sliderField.insertAdjacentElement('afterbegin', elemFirst)
+        
+    //         sliderField.style.width = `${step * (slides.length + 2)}px`
+    //         sliderField.style.transform = `translateX(${-step}px)`
+        
+    //         rightBtn.addEventListener('click', () => {
+        
+    //             sliderField.style.transition = `all ${speed}ms ease-out`
+    //             sliderField.style.transform = `translateX(-${step * 2}px)`
+    //             rightBtn.disabled = true
+    //             const slides2 = document.querySelectorAll(slidesSelector)
+    //             setTimeout(() => {
+    //                 const elemLast = slides2[2].cloneNode(true)
+    //                 sliderField.append(elemLast)
+    //                 slides2[0].remove()
+    //                 sliderField.style.transition = ''
+    //                 rightBtn.disabled = false
+    //                 sliderField.style.transform = `translateX(-${step}px)`
+    //             }, speed)
+    //         })
+    //         leftBtn.addEventListener('click', () => {
+    //             sliderField.style.transition = `all ${speed}ms ease-out`
+    //             sliderField.style.transform = `translateX(0px)`
+    //             leftBtn.disabled = true
+    //             const slides2 = document.querySelectorAll(slidesSelector)
+    //             setTimeout(() => {
+        
+    //                 const elemFirst = slides2[slides2.length - 3].cloneNode(true)
+    //                 sliderField.insertAdjacentElement('afterbegin', elemFirst)
+    //                 slides2[slides2.length - 1].remove()
+    //                 leftBtn.disabled = false
+    //                 sliderField.style.transition = ''
+    //                 sliderField.style.transform = `translateX(-${step}px)`
+        
+    //             }, speed)
+    //         })
+    //     } else {
+    //         leftBtn.style.display = 'none'
+    //         rightBtn.style.display = 'none'
+    //     }
+    
+        
+    // }
+    
+    // // sliderMain('.inner-slider__wrapper', '.inner-slider__field', '.inner-slider__slide', '.inner-slider__left', '.inner-slider__rigth', 'inner-slider__slide',500)
+    
+    // sliderSmall('.products-slider__field', '.product-slider__item', '.products-slider__left', '.products-slider__right', 500, '.products-slider__wrapper')
+    // sliderSmall('.inner-slider__field', '.inner-slider__slide', '.inner-slider__left', '.inner-slider__right', 500, '.inner-slider__wrapper', true)
+    
+    
+    function slider(wrapperSelector, fieldSelector, slidesSelector, leftSelector, rightSelector, speed = 300, fullWidth = null) {
         const sliderField = document.querySelector(fieldSelector)
         const slides = document.querySelectorAll(slidesSelector)
         const leftBtn = document.querySelector(leftSelector)
         const rightBtn = document.querySelector(rightSelector)
         const wrapper = document.querySelector(wrapperSelector)
         let step = 0
-        let widthSlides = slides[0].clientWidth * slides.length
+        let count = 0
+        let endSlide = 0
+        let slideLength = slides.length
+        let slideWidth
+        
 
-        if(adaptiv) {
-            step = wrapper.clientWidth
-            widthSlides = step * slides.length
+        if (fullWidth) {
+            slideWidth = wrapper.clientWidth
+            endSlide = slides.length - 1
             window.addEventListener('resize', () => {
-                step = wrapper.clientWidth
-
-                widthSlides = step * slides.length
-
-                sliderField.style.width = `${step * (slides.length + 2)}px`
-                sliderField.style.transform = `translateX(${-step}px)`
+                slideWidth = wrapper.clientWidth
+                sliderField.style.width = `${slideLength * slideWidth}px`
+                sliderField.style.transform = `translate(${-slideWidth * count}px)`
             })
-            
         } else {
-            step = slides[0].clientWidth
-            window.addEventListener('resize', () => {
-                step = slides[0].clientWidth
-                sliderField.style.transform = `translateX(${-step}px)`
-            })
-        }
-        
-        if(widthSlides >= (wrapper.clientWidth)) {
+            slideWidth = slides[0].clientWidth
+            endSlide = Math.floor(wrapper.clientWidth / slideWidth)
+            endSlide = slideLength - endSlide
             
-            const elemLast = slides[0].cloneNode(true)
-            sliderField.append(elemLast)
-        
-            const elemFirst = slides[slides.length - 1].cloneNode(true)
-            sliderField.insertAdjacentElement('afterbegin', elemFirst)
-        
-            sliderField.style.width = `${step * (slides.length + 2)}px`
-            sliderField.style.transform = `translateX(${-step}px)`
-        
-            rightBtn.addEventListener('click', () => {
-        
-                sliderField.style.transition = `all ${speed}ms ease-out`
-                sliderField.style.transform = `translateX(-${step * 2}px)`
+        }
+
+        sliderField.style.width = `${slideLength * slideWidth}px`
+
+        rightBtn.addEventListener('click', () => {
+            sliderField.style.transition = `transform ${speed}ms ease-out`
+            rightBtn.disabled = true
+            count++
+            if(count >= endSlide) {
                 rightBtn.disabled = true
-                const slides2 = document.querySelectorAll(slidesSelector)
+                rightBtn.style.opacity = 0.5
                 setTimeout(() => {
-                    const elemLast = slides2[2].cloneNode(true)
-                    sliderField.append(elemLast)
-                    slides2[0].remove()
-                    sliderField.style.transition = ''
+                    sliderField.style.transition = ``
+                }, speed)
+            } else {
+                setTimeout(() => {
+                    sliderField.style.transition = ``
                     rightBtn.disabled = false
-                    sliderField.style.transform = `translateX(-${step}px)`
                 }, speed)
-            })
-            leftBtn.addEventListener('click', () => {
-                sliderField.style.transition = `all ${speed}ms ease-out`
-                sliderField.style.transform = `translateX(0px)`
+            }
+            leftBtn.disabled = false
+            leftBtn.style.opacity = 1
+            step = count * slideWidth
+            sliderField.style.transform = `translate(${-step}px)`
+
+        })
+
+        leftBtn.disabled = true
+        leftBtn.addEventListener('click', () => {
+            count--
+            leftBtn.disabled = true
+            sliderField.style.transition = `transform ${speed}ms ease-out`
+            if(count == 0) {
                 leftBtn.disabled = true
-                const slides2 = document.querySelectorAll(slidesSelector)
+                leftBtn.style.opacity = 0.5
+            } else {
                 setTimeout(() => {
-        
-                    const elemFirst = slides2[slides2.length - 3].cloneNode(true)
-                    sliderField.insertAdjacentElement('afterbegin', elemFirst)
-                    slides2[slides2.length - 1].remove()
+                    sliderField.style.transition = ``
                     leftBtn.disabled = false
-                    sliderField.style.transition = ''
-                    sliderField.style.transform = `translateX(-${step}px)`
-        
                 }, speed)
-            })
-        } else {
-            leftBtn.style.display = 'none'
-            rightBtn.style.display = 'none'
-        }
-    
-        
+            }
+            rightBtn.disabled = false
+            rightBtn.style.opacity = 1
+            step = count * slideWidth
+            sliderField.style.transform = `translate(${-step}px)`
+            setTimeout(() => {
+                sliderField.style.transition = ``
+            }, speed)
+        })
     }
-    
-    // sliderMain('.inner-slider__wrapper', '.inner-slider__field', '.inner-slider__slide', '.inner-slider__left', '.inner-slider__rigth', 'inner-slider__slide',500)
-    
-    sliderSmall('.products-slider__field', '.product-slider__item', '.products-slider__left', '.products-slider__right', 500, '.products-slider__wrapper')
-    sliderSmall('.inner-slider__field', '.inner-slider__slide', '.inner-slider__left', '.inner-slider__right', 500, '.inner-slider__wrapper', true)
-    
+
+    slider('.inner-slider__wrapper', '.inner-slider__field', '.inner-slider__slide', '.inner-slider__left', '.inner-slider__right', 500, true)
+    slider('.products-slider__wrapper', '.products-slider__field', '.product-slider__item', '.products-slider__left', '.products-slider__right', 500)
+    slider('.custome__wrapper', '.custome__field', '.custome__item', '.custome-slider__left', '.custome-slider__right', 300)
+    slider('.msi-laptops__wrapper', '.msi-laptops__field', '.msi-laptops__item', '.msi-laptops__left', '.msi-laptops__right', 500)
+    slider('.laptops__wrapper', '.laptops__field', '.laptops__item', '.desctops-slider__left', '.desctops-slider__right', 500)
+    slider('.monitors__wrapper', '.monitors__field', '.monitors__item', '.monitors__left', '.monitors__right', 500)
+
+
+    //табы с помощью класса
+    class Tabs {
+        constructor(tabNamesSelector, tabBtnSelector, tabContentSelector) {
+            this.tabsNames = document.querySelectorAll(tabNamesSelector)
+            this.tabBtn = document.querySelector(tabBtnSelector)
+            this.tabContent = document.querySelectorAll(tabContentSelector)
+            this.tabClass = tabNamesSelector.slice(1)
+
+            this.hideTabs = this.hideTabs.bind(this)
+            this.showTabs = this.showTabs.bind(this)
+            this.render = this.render.bind(this)
+
+            this.hideTabs()
+            this.showTabs()
+            this.render()
+        }
+
+        hideTabs() {
+            this.tabContent.forEach(item => {
+                item.classList.add('tabs_hide')
+                item.classList.remove('tabs_show', 'opac-anim')
+            })
+
+            this.tabsNames.forEach(item => {
+                item.classList.remove('tabs_active')
+            })
+        }
+
+        showTabs(i = 0) {
+            this.tabContent[i].classList.remove('tabs_hide')
+            this.tabContent[i].classList.add('tabs_show', 'opac-anim')
+
+            this.tabsNames[i].classList.add('tabs_active')
+        }
+
+        render() {
+            this.tabBtn.addEventListener('click', e => {
+                if(e.target && e.target.classList.contains(this.tabClass)) {
+                    this.tabsNames.forEach((item, i) => {
+                        if(e.target == item) {
+                            this.hideTabs()
+                            this.showTabs(i)
+                        }
+                    })
+                } 
+            })
+        }
+    }
+
+    new Tabs('.msi-category', '.msi-categories', '.msi-laptops__slider')
     
 })
